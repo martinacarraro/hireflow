2d855013479dbcd3c76391e2b2fe7e9b24574853
martinacarraro <martina.carraro00@gmail.com>
2026-03-16 22:52:45 +0000

    fix: import LOADING_TIPS in Splash

--- candidature-app/candidature-app/src/screens/Splash.jsx
+++ candidature-app/candidature-app/src/screens/Splash.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { LOADING_TIPS, LOADING_TIPS_EN, randomInt } from '../lib/utils'
 
 export default function Splash({ onDone }) {
   const { t, i18n } = useTranslation()
-  const tips = LOADING_TIPS
+  const tips = i18n.language === 'en' ? LOADING_TIPS_EN : LOADING_TIPS
   const [tip] = useState(() => tips[randomInt(0, tips.length - 1)])
   const [progress, setProgress] = useState(0)
 
